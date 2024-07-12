03a98b1be2b4c3621afaada36aa9dd63fa2ee2e7
aiden0921an <nelsonaiden23@gmail.com>
2024-07-11 19:14:12 -0500

    added to modal, started js

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,50 +1,25 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const modalSubmit= $('#modalSubmit')
+const taskTitleInput= $('#task-title')
+const taskDateInput= $('#task-date')
+const taskDescInput= $('#task-desc')
+
+
+function saveTaskLocal(tasks){
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-return JSON.parse
+function generateTaskId() { 
+
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const taskCard = $('<div>')
-    .addClass('card project-card draggable my-3')
-    .attr('data-project-id', project.id);
-  const cardHeader = $('<div>').addClass('card-header h4').text(project.name);
-  const cardBody = $('<div>').addClass('card-body');
-  const cardDescription = $('<p>').addClass('card-text').text(project.type);
-  const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
-  const cardDeleteBtn = $('<button>')
-    .addClass('btn btn-danger delete')
-    .text('Delete')
-    .attr('data-project-id', project.id);
-  cardDeleteBtn.on('click', handleDeleteProject);
-
-// ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
-if (project.dueDate && project.status !== 'done') {
-    const now = dayjs();
-    const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
-
-
-
 }
-    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
-    if (now.isSame(taskDueDate, 'day')) {
-        taskCard.addClass('bg-warning text-white');
-      } else if (now.isAfter(taskDueDate)) {
-        taskCard.addClass('bg-danger text-white');
-        cardDeleteBtn.addClass('border-light');
-      }
-    }
-  
-    // ? Gather all the elements created above and append them to the correct elements.
-    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
-    taskCard.append(cardHeader, cardBody);
-  
-    // ? Return the card so it can be appended to the correct lane.
-    return taskCard;
 
 
 
@@ -55,6 +30,7 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+  event.preventDefault();
 
 }
 
@@ -72,3 +48,4 @@ submitBtn
 $(document).ready(function () {
 
 });
+
